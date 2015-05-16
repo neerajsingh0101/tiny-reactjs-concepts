b37a6920ee31f1bc8b18f4f5a0cfc03331be922f
Neeraj Singh <neeraj@bigbinary.com>
2015-05-15 22:03:13 -0400

    added display list component

---
+++ todo-app/5-displaylist-component/app.js
@@ -0,0 +1,51 @@
+var React = require('react');
+
+var DisplayList = React.createClass({
+
+  displayItem (item) {
+    return <li key={item}>{ item }</li>
+  },
+
+  render () {
+    return (
+      <ul>{ this.props.items.map(this.displayItem) }</ul>
+    )
+  }
+});
+
+var App = React.createClass({
+
+  getInitialState () {
+    return { text: '', items: [] }
+  },
+
+  handleSubmit (event) {
+    event.preventDefault();
+
+    var text = this.state.text;
+    var newItems = this.state.items.concat(text);
+    this.setState({ text: '', items: newItems });
+  },
+
+  handleChange (event) {
+    var text = event.target.value;
+    this.setState({ text: text });
+  },
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div> TODO </div>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button> Submit </button>
+        </form>
+
+        <DisplayList items={this.state.items} />
+      </div>
+    );
+  }
+
+});
+
+React.render(<App />, document.body);
