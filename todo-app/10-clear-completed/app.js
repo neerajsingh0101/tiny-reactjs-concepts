89821f21090105f73cb3cfb7da9b5264c447fbf8
Neeraj Singh <neeraj@bigbinary.com>
2015-05-19 14:37:18 -0400

    clear completed

---
+++ todo-app/10-clear-completed/app.js
@@ -0,0 +1,89 @@
+var React = require('react');
+
+var DisplayItem = React.createClass({
+
+  render () {
+    var item = this.props.item;
+    return <li key={item}>
+            <input type="checkbox" checked={item.done} onChange={this.props.handleDone.bind(null, item.text)} />
+            { item.text }
+            <a href='#' onClick={this.props.handleDelete.bind(null, item.text)}>[x]</a>
+           </li>
+  }
+
+})
+
+var DisplayList = React.createClass({
+
+  displayItem (item) {
+    return <DisplayItem item={item} handleDelete={this.props.handleDelete} handleDone={this.props.handleDone} />
+  },
+
+  render () {
+    return <ul>{ this.props.items.map(this.displayItem) }</ul>
+  }
+
+});
+
+var App = React.createClass({
+
+  getInitialState () {
+    return { text: '', items: [] }
+  },
+
+  handleDone (taskName) {
+    var _items = this.state.items;
+    var item = _items.filter((item) => { return item.text == taskName } )[0];
+    item.done = !item.done;
+    this.setState({ items: _items });
+  },
+
+  handleDelete (taskName) {
+    var newItems = this.state.items.filter((item) => { return item.text != taskName } );
+    this.setState({ items: newItems });
+  },
+
+  handleSubmit (event) {
+    event.preventDefault();
+
+    var text = this.state.text;
+    var newItems = this.state.items.concat({ text: text, done: false});
+    this.setState({ text: '', items: newItems });
+  },
+
+  handleChange (event) {
+    var text = event.target.value;
+    this.setState({ text: text });
+  },
+
+  handleClearCompleted () {
+    var newItems = this.state.items.filter((item) => { return !item.done } );
+    this.setState({ items: newItems });
+  },
+
+  render () {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div> TODO </div>
+          <input onChange={this.handleChange} value={this.state.text} />
+          <button> Submit </button>
+        </form>
+
+        <p>
+          All({ this.state.items.length }) |
+          Completed({ this.state.items.filter((item) => { return item.done }).length })
+          Pending({ this.state.items.filter((item) => { return !item.done }).length }) |
+          <a href='#' onClick={this.handleClearCompleted}>Clear Completed</a>
+        </p>
+
+        <DisplayList items={this.state.items}
+                     handleDone={this.handleDone}
+                     handleDelete={this.handleDelete} />
+      </div>
+    );
+  }
+
+});
+
+React.render(<App />, document.body);
